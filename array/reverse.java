b918d111ad130bea35549852f3745b833e68aa32
amalbabu0 <amalbabu2803@gmail.com>
2026-04-23 22:25:31 +0530

    .

---
+++ array/reverse.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class reverse {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter limit");
+        int n = s.nextInt();
+        int a[] = new int [n];
+        int b[] = new int [n];
+        System.out.println("enter " + n + "values");
+        for( int i=0; i<n; i++) {
+            a[i] = s.nextInt();
+        }
+        int left = 0;
+        int right = a.length - 1;
+        while (left < right) {
+            int temp = a[left];
+            a[left] = a[right];
+            a[right] = temp;
+            left++;
+            right--;
+        }
+        for( int i=0; i<n; i++) {
+        System.out.print(a[i]);
+        }
+    }
+}
